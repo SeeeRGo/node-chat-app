6c556fdbe56bb1c4e28050c177b6ceb765abbf9d
SeeeRGo <seeergo@email.com>
2017-11-25 00:11:34 +0400

    Geolocation feature added

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,55 @@
 var socket = io();
 
 
-		socket.on('connect', function () {
-			console.log('Connected to server');
-		});
+socket.on('connect', function () {
+	console.log('Connected to server');
+});
 
-		socket.on('disconnect', function () {
-			console.log('disConnected from server');
-		});
+socket.on('disconnect', function () {
+	console.log('disConnected from server');
+});
 
-		socket.on('newMessage', function(newMessage) {
-			console.log('New Message', newMessage)
-			var li = jQuery('<li></li>')
-			li.text(`${newMessage.from}: ${newMessage.text}`)
+socket.on('newMessage', function(newMessage) {
+	console.log('New Message', newMessage)
+	var li = jQuery('<li></li>')
+	li.text(`${newMessage.from}: ${newMessage.text}`)
 
-			jQuery('#messages').append(li)
+	jQuery('#messages').append(li)
+})
+
+socket.on('newLocationMessage', function(message) {
+	var li = jQuery('<li></li>')
+	var a = jQuery('<a target="_blank">My current location</a>')
+
+	li.text(`${message.from}: `)
+	a.attr('href', message.url)
+
+	li.append(a)
+	jQuery('#messages').append(li)
+})
+
+jQuery('#message-form').on('submit', function(e) {
+	e.preventDefault()
+
+	socket.emit('createMessage' , {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function(){
+
+	})
+})
+
+var locationButton = jQuery('#send-location')
+locationButton.on('click', function() {
+	if(!navigator.geolocation){
+		return alert('Geolocation not supported by your browser')
+	} 
+	navigator.geolocation.getCurrentPosition(function (position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
 		})
-
-		jQuery('#message-form').on('submit', function(e) {
-			e.preventDefault()
-
-			socket.emit('createMessage' , {
-				from: 'User',
-				text: jQuery('[name=message]').val()
-			}, function(){
-
-			})
-		})
+	}, function() {
+		alert('Unable to fetch location')
+	})
+})
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect')
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -9,5 +9,16 @@ describe('generateMessage', () => {
 		expect(message.from).toBe(from)
 		expect(message.text).toBe(text)
 		expect(message.createdAt).toBeA('number')
+	})
+})
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var lat = 1
+		var lng = 2
+		var from = 'Bot'
+		var location = generateLocationMessage(from, lat, lng)
+		expect(location.from).toBe(from)
+		expect(location.url).toBe(`https://www.google.com/maps?q=${lat},${lng}`)
+		expect(location.createdAt).toBeA('number')
 	})
 })
