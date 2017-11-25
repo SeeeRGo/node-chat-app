d59098e35b649cc134520a4cc41574327af6fb4f
SeeeRGo <seeergo@email.com>
2017-11-25 23:58:47 +0400

    Chat scroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom () {
+	var messages = jQuery('#messages')
+	var newMessage = messages.children('li:last-child')
+
+	var clientHeight = messages.prop('clientHeight')
+	var scrollTop = messages.prop('scrollTop')
+	var scrollHeight = messages.prop('scrollHeight')
+	var newMessageHeight = newMessage.innerHeight()
+	var lastMessageHeight = newMessage.prev().innerHeight()
+	var totalHeight = clientHeight + scrollTop + newMessageHeight + lastMessageHeight
+	if(totalHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight)
+	}
+}
 
 socket.on('connect', function () {
 	console.log('Connected to server');
@@ -11,22 +25,27 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(newMessage) {
 	var formattedTime = moment(newMessage.createdAt).format('h:mm a')
-	var li = jQuery('<li></li>')
-	li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`)
-
-	jQuery('#messages').append(li)
+	var template = jQuery('#message-template').html()
+	var html = Mustache.render(template, {
+		text: newMessage.text,
+		from: newMessage.from,
+		createdAt: formattedTime
+	})
+	jQuery('#messages').append(html)
+	scrollToBottom()
 })
 
 socket.on('newLocationMessage', function(message) {
-	var formattedTime = moment(newMessage.createdAt).format('h:mm a')
-	var li = jQuery('<li></li>')
-	var a = jQuery('<a target="_blank">My current location</a>')
-
-	li.text(`${message.from} ${formattedTime}: `)
-	a.attr('href', message.url)
-
-	li.append(a)
-	jQuery('#messages').append(li)
+	var formattedTime = moment(message.createdAt).format('h:mm a')
+	var template = jQuery('#location-message-template').html()
+	console.log(template)
+	var html = Mustache.render(template, {
+		url: message.url,
+		from: message.from,
+		createdAt: formattedTime
+	})
+	jQuery('#messages').append(html)
+	scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', function(e) {
